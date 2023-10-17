1bda0ccfc0437badef99c45cec26d798d21bcd81
Deniz Ök <deniz.ok2019@gmail.com>
2023-10-17 17:05:16 +0300

    fixed scroll and added login page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='flex items-center justify-center p-4 mt-5 md:px-20 xl:px-40'>
+        <div className='bg-[#1e2333] p-10 flex md:w-[80%] lg:w-[50%] flex-col items-center justify-center gap-12 rounded-md text-white'>
+            <div className='bg-[#ff5555] text-center p-2 lg:p-3 cursor-pointer rounded-md font-bold min-w-[190px] lg:min-w-[200px]'>Sign in with Google</div>
+            <div className='bg-[#111] text-center p-2 lg:p-3 cursor-pointer rounded-md font-bold min-w-[190px] lg:min-w-[200px]'>Sign in with Github</div>
+            <div className='bg-[#087BEA] text-center p-2 lg:p-3 cursor-pointer rounded-md font-bold min-w-[190px] lg:min-w-[200px]'>Sign in with Facebook</div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginPage
--- src/components/ReadMore.jsx
+++ src/components/ReadMore.jsx
@@ -1,16 +1,20 @@
 "use client"
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const ReadMore = ({ children }) => { 
   const text = children; 
   const [isReadMore, setIsReadMore] = useState(true); 
+  const divRef = useRef(null);
 
   const toggleReadMore = () => { 
-    setIsReadMore(!isReadMore); 
-
+    setIsReadMore(!isReadMore);
+    if (!isReadMore && divRef.current) {
+      window.scrollTo({ top: divRef.current.offsetTop -100, behavior: 'smooth' });
+    }
   }; 
+ 
   return ( 
-    <p className="text"> 
+    <p className="text" ref={divRef}> 
       {isReadMore ? text.slice(0, 150) : text} 
       <span onClick={toggleReadMore} className="read-or-hide cursor-pointer"> 
         {isReadMore ? "...Read more" : " Show less"} 
