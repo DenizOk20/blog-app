aafd9d48959cbd8029fc861249444428e59998f5
Deniz Ök <deniz.ok2019@gmail.com>
2024-03-15 20:57:47 +0300

    fetch single post with user info

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,43 +4,45 @@ import ReadMore from '@/components/ReadMore'
 import Image from 'next/image'
 import React from 'react'
 
-const singlePage = () => {
+const getData = async (slug) => {
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`)
+
+    if(!res.ok){
+        throw new Error("failed")
+    }
+    
+    return res.json()
+}
+
+
+const singlePage = async ({params}) => {
+
+    const {slug} = params
+
+    const data = await getData(slug)
+    console.log(data?.user)
   return (
-    <div className='p-4 md:px-20 xl:px-40 flex flex-col'>
+        <div className='p-4 md:px-20 xl:px-40 flex flex-col'>
         <div className='h-[80vh] md:h-[50vh] lg:h-[45vh] flex flex-col gap-7 md:flex-row'>
             <div className='flex flex-col h-1/3 md:h-full md:w-1/2 lg:w-1/2 gap-4 md:justify-between'>
-                <h1 className='font-bold text-3xl md:text-5xl lg:text-6xl'>Lorem tempora obcaecati, animi voluptatem.</h1>
+                <h1 className='font-bold text-3xl md:text-5xl lg:text-6xl'>{data.title}</h1>
                 <div className='flex items-center gap-2'>
-                    <Image alt='' src="/p1.jpeg" width={64} height={64} className='w-12 h-12 rounded-full'/>
+                    <Image alt='' src={`${data?.user?.image}`} width={64} height={64} className='w-12 h-12 rounded-full'/>
                     <div>
-                        <p>Mehmet Fatih</p>
-                        <span className='font-light'>25 April 2023</span>
+                        <p>{data?.user?.name}</p>
+                        <span className='font-light'>{data?.createdAt.substring(0,10)}</span>
                     </div>
                 </div>
             </div>
-            <div className='relative h-2/3 w-full md:h-full lg:w-1/2'>
+            {data?.img && <div className='relative h-2/3 w-full md:h-full lg:w-1/2'>
                 <Image alt='' src="/p1.jpeg" fill className='w-full rounded-md object-cover'/>
-            </div>
+            </div>}
         </div>
         <div className='flex flex-col lg:flex-row pt-10'>
             <div className='flex flex-col gap-8 lg:w-1/2 p-4'>
-                <ReadMore className=''>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque nam corrupti unde, explicabo, quia eum rem, aliquam 
-                    ipsa harum saepe repellendus placeat. Dolores eum, suscipit delectus ullam totam aut quod?
-                Nobis tempora cumque obcaecati nemo autem perspiciatis doloremque repellendus asperiores animi?
-                 Iure rem amet officia quo adipisci eaque iusto dignissimos debitis voluptates exercitationem atque vero sed totam 
-                 veritatis, itaque tempore.
-                Autem dolor, enim modi repellendus id earum atque amet minima doloribus aliquid fugit excepturi corporis laborum
-                 similique expedita perspiciatis ex cupiditate blanditiis soluta repudiandae dignissimos commodi, vitae est deserunt!
-                  Est.
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt minus ex, ullam quis laudantium porro facilis
-                 delectus voluptatum praesentium ipsum quod placeat aliquid, cupiditate fuga rerum aperiam? Ratione, numquam inventore?
-                Ut sunt, molestias minima libero nulla harum nostrum eos quam tenetur labore! Placeat aliquid atque iusto cupiditate,
-                 ratione doloribus necessitatibus aut, voluptates, facere delectus eveniet maiores. Consectetur veritatis repudiandae nemo.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quisquam rerum consectetur quod culpa, tempora officiis 
-                libero voluptate sequi, natus quia, totam dignissimos est atque accusantium quos voluptas aspernatur vitae.
-                Neque dolorem, magnam, illum mollitia, quis expedita cum quod sint atque explicabo repellendus ipsa doloremque obcaecati
-                 quasi tempora dicta aperiam. Odit quasi nesciunt at distinctio itaque rem minima temporibus quae.
-                 </ReadMore>
+                <div dangerouslySetInnerHTML={{__html:data?.desc}}>
+
+                </div>
                  <div className=''>
                     <Comments/>
                  </div>
